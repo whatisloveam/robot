fdd0afc7616642e7aac6bba9c7ca43eb5f30c8d6
Vladislav <melnikow.va@gmail.com>
2021-02-09 19:01:14 +0500

    added line follow

--- robot/robot.c
+++ robot/robot.c
@@ -1,4 +1,15 @@
 // 192.168.31.15:7070
+// github.com/whatisloveam/robot
+
+void line() {
+	while(true) 
+	{
+		int u = -0.3 *(analogRead(A0) - analogRead(A1));
+		motorB(150 + u);
+		motorC(150 - u);
+		delay(10);
+	}
+}
 
 void setup()
 {
@@ -6,16 +17,10 @@ void setup()
 	pinMode(5, OUTPUT);
 	pinMode(6, OUTPUT);
 	pinMode(7, OUTPUT);
-	
-	moveForTime(150, 2000);
-	motorBForTime(150, 1000);
-	moveForTime(150, 2000);
-	motorBForTime(150, 1000);
-	moveForTime(150, 2000);
-	motorBForTime(150, 1000);
-	moveForTime(150, 2000);
-	motorBForTime(150, 1000);
+	line();
 }
+
+
 
 void loop()
 {	
